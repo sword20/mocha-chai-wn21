1d26cb4e80dbdfc461dad3c57f7864d88ff8df36
Replit user <replituser@example.com>
2021-03-23 21:56:05 +0000

    adds test

---
+++ Calculator.js
@@ -0,0 +1,16 @@
+class Calculator{
+
+  constructor()
+  {
+	console.log("Calculator loaded");
+  }
+
+  add(num1, num2){
+    return num1 + num2;
+    }
+  
+
+}
+
+//required so data is visible externally
+module.exports = Calculator;
